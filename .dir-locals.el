69783c2b06da988a9c5c627e34805d77a19f9e5f
Diego Berrocal <cestdiego@gmail.com>
2015-05-29 14:47:34 -0500

    better quotes

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((firestarter . (op/do-publication
+                         t
+                         t
+                         "~/Projects/le_blog_built"))))
+ (".git" . ((firestarter . nil))))
